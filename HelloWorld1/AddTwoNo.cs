a65d10b49dcfc1de8d61dfc2fb2895782be130db
Shree Thakre <shreet365@gmail.com>
2022-12-17 11:12:49 +0530

    initial commit

---
+++ HelloWorld1/AddTwoNo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld1
+{
+    class AddTwoNo
+    {
+        public static void Add()
+        {
+            Console.WriteLine("Enter 1st Number");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter 2nd Number");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int c = a + b;
+            Console.WriteLine("Addition is :" + c);
+        }
+    }
+}
